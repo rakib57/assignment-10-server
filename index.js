2538434c29c28689f94c6c428319c9082dfbc9d3
nipun006 <67517505+nipun006@users.noreply.github.com>
2020-12-02 14:48:08 +0600

    port update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 require('dotenv').config()
 
-const uri = `mongodb+srv://${process.env.DB_User}:${process.env. DB_Pass}@cluster0.aea4t.mongodb.net/${process.env. DB_Name}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env. DB_PASS}@cluster0.aea4t.mongodb.net/${process.env. DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
